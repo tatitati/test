be8dfe082501e1fc7d2eac34acfe716007f529f3
experimentos <experimentos@francisco.local>
2016-10-19 21:19:12 +0100

    Configured and refactored index bootstraping

--- radio_service/app/public/index.php
+++ radio_service/app/public/index.php
@@ -12,42 +12,15 @@ if (PHP_SAPI == 'cli-server') {
 
 session_start();
 
-// Instantiate the app
-$settings = require __DIR__ . '/../src/settings.php';
+$settings        = require __DIR__ . '/../src/settings.php';
+$app             = new \Slim\App($settings);
+$pimpleContainer = new Pimple\Container();
 
-$app       = new \Slim\App($settings);
-$container = $app->getContainer();
-
-// REGISTER TWIG TEMPLATES
-$container['view'] = function ($container) {
-    $view = new \Slim\Views\Twig(__DIR__ . '/../templates', [
-        'cache' => __DIR__ . '/cache'
-    ]);
-    $view->addExtension(new \Slim\Views\TwigExtension(
-        $container['router'],
-        $container['request']->getUri()
-    ));
-
-    return $view;
-};
-
-// Set up dependencies
+require __DIR__ . '/../src/container.php';
 require __DIR__ . '/../src/dependencies.php';
-
-// Register middleware
 require __DIR__ . '/../src/middleware.php';
-
-// Register routes
 require __DIR__ . '/../src/routes.php';
-require __DIR__ . '/../controllers/Product.php';
-
-
-// load controllerss
-//foreach(glob(__DIR__ . "/../controllers/*.php") as $file){
-//    require $file;
-//}
 
 
 
-// Run app
 $app->run();
