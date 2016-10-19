065b2617c355966d90fdba7ae354330effa8d30e
experimentos <experimentos@francisco.local>
2016-10-19 21:18:37 +0100

    Added Pimple container DI and cleaned bootstrap file

--- radio_service/app/src/routes.php
+++ radio_service/app/src/routes.php
@@ -1,10 +1,5 @@
 <?php
-// Routes
-
-$app->get('/[{name}]', function ($request, $response, $args) {
-    // Sample log message
-    $this->logger->info("Slim-Skeleton '/' route");
-
-    // Render index view
-    return $this->renderer->render($response, 'index.phtml', $args);
-});
+foreach (glob(__DIR__ . "/../controllers/*.php") as $filename)
+{
+    require $filename;
+}
