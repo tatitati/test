919192e7c588a9d0032e6b622829dde149da2ed4
experimentos <experimentos@francisco.local>
2016-10-19 21:21:18 +0100

    Refactoring in controller

--- radio_service/app/controllers/ProductController.php
+++ radio_service/app/controllers/ProductController.php
@@ -1,16 +1,10 @@
 <?php
-
-use \Psr\Http\Message\ServerRequestInterface as Request;
-use \Psr\Http\Message\ResponseInterface as Response;
-use GuzzleHttp\Client;
 use Bbc\Radio\Services\BbcRadioService;
-use Slim\App;
-
 
 $app->get('/product/{search_string}',  function ($request, $response) use ($pimpleContainer) {
         /** @var BbcRadioService $serviceBbcRadio */
         $att = $request->getAttribute('search_string');
-
+        /** @var BbcRadioService $serviceBbcRadio */
 		$serviceBbcRadio = $pimpleContainer['service_bbc_radio'];
         return $serviceBbcRadio->searchByString($att);
 });
