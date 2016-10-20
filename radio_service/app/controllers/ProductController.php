c034258e937fef9fe884715c7e7b5593b2c04a91
experimentos <experimentos@francisco.local>
2016-10-20 18:38:24 +0100

    Added controller with whole functionality

--- radio_service/app/controllers/ProductController.php
+++ radio_service/app/controllers/ProductController.php
@@ -2,9 +2,16 @@
 use Bbc\Radio\Services\BbcRadioService;
 
 $app->get('/product/{search_string}',  function ($request, $response) use ($pimpleContainer) {
-        /** @var BbcRadioService $serviceBbcRadio */
         $att = $request->getAttribute('search_string');
+
         /** @var BbcRadioService $serviceBbcRadio */
 		$serviceBbcRadio = $pimpleContainer['service_bbc_radio'];
-        return $serviceBbcRadio->searchByString($att);
+        $searchResults   = $serviceBbcRadio->searchByString($att);
+        $results         = json_decode($searchResults, true);
+
+        if (isset($results['atoz']['tleo_titles']) && !empty($results['atoz']['tleo_titles'])) {
+                return json_encode($results['atoz']['tleo_titles']);
+        }
+
+        return 'Not found';
 });
